b1cfe07c8883af2f9bc31d5f7f57bf97b4d07aff
Yonis Savary <yonissavary@outlook.fr>
2020-08-10 16:55:23 +0200

    added cascade-cursor 'plugin' + tooltips scripts + forced Css button

--- public/js/cascade.js
+++ public/js/cascade.js
@@ -1,12 +1,16 @@
-/*****************************************************
-	Apply the default css code
-*****************************************************/
+// ! CASCADE JS NEED JQUERY TO WORK !
+
+/* GLOBAL VARIABLES */
+var COMPONENTS = {};
+/* CASCADE PLUGINS ROUTINE */
+if (typeof(CASCADE_PLUGINS) === "undefined") var CASCADE_PLUGINS = {};
 
 const DEFAULT_CSS = `:root
 {
 	font-size: 24px;
 	font-family: "sans-serif";
-}`
+}`;
+
 const DEFAULT_HTML = `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,103 +22,155 @@ const DEFAULT_HTML = `<!DOCTYPE html>
 	<style id="cascadeCss"></style>
 	<h1>Yay ! A new document !</h1>
 	<!-- Some content here -->
-	<!-- NO NEED TO INCLUDE THE CSS - YOUR CSS WILL BE INCLUDED IN #cascadeCss -->
 </body>
-</html>`
+</html>`;
 
-$("#htmlEditor").val(DEFAULT_HTML);
-$("#cssEditor").val(DEFAULT_CSS);
+
+/* GLOBAL VARIABLES FUNCTIONS */
+/* 'COMPONENTS' GLOBAL VARIABLE CONTAIN ALL OF THE BUTTONS AND INTERFACES
+ * ELEMENTS, SO WE EXECUTE ONLY ONE TIME ALL OF THE JQUERY SELECTION AND
+ * STORE THEM IN 'COMPONENTS'
+*/
+
+function addComponentBySelector(name, selector)
+{
+    let obj = $(selector)
+    if (obj != null) COMPONENTS[name] = $(selector);
+    return COMPONENTS[name];
+}
+
+function addComponent(name, object)
+{
+    COMPONENTS[name] = object;
+}
+
+/* 
+    IF ANOTHER SCRIPT REGISTER ITSELF AS A PLUGIN, THIS FUNCTION
+    CALL ITS "perform" FUNCTION AND CONCAT ALL OF THE RESULTS
+*/
+
+function getPluginsContent(cssCode)
+{
+	let returnCss = ""
+	for (pluginName in CASCADE_PLUGINS)
+	{
+		let plugins = CASCADE_PLUGINS[pluginName];
+		if (plugins.enabled && plugins.active)
+		{
+			let newCss = (plugins.perform)(cssCode);
+			returnCss += newCss;
+		}
+	}
+	return returnCss;
+}
 
 /*****************************************************
   User choice and UI
 *****************************************************/
 var buildIntervalObject = null;
-$("#testIntervalSelect").on("change",(e)=>{
-    let interval = $("#testIntervalSelect").val();
-    if (interval == -1)
-    {
-        $("#manualBuildButton").show();
-        clearInterval(buildIntervalObject);
-    }
-    else
-    {
-        $("#manualBuildButton").hide();
-        clearInterval(buildIntervalObject);
-        buildIntervalObject = setInterval(build, interval);
-    }
-})
-$("#testIntervalSelect").val(-1);
-$("#manualBuildButton").on('click', build);
-
-/*****************************************************
-  Iframe filling
-*****************************************************/
-var errorFound = false;
-function build(verbose=true)
+function initIntervalUI()
 {
-	errorFound = false;
-	console.log('build...')
-	// get html & css codes from mirrors objects
-	let htmlCode = mirrors["htmlEditor"].getValue();
-	let cssCode = mirrors["cssEditor"].getValue();
-
-	// build the html code
-	let parser = new DOMParser();
-	// transform the html textarea value into a dom object
-    let doc = parser.parseFromString(htmlCode, "text/html"); 
-    if (doc.querySelector("#cascadeCss") !== null)
-    {
-		// insert the css textarea value into the new dom object
-        doc.querySelector("#cascadeCss").innerHTML = cssCode;
-    }
-    else
-    {
-		// show warning if necessary
-		errorFound = true;
-        if (verbose) { $("#warningSpan").text(" - Balise style #cascadeCss non trouvée") }
-    }
-	let sourceHtml = `<html>${doc.documentElement.innerHTML}</html>`
-
-	// writing html code into the iframe
-	let iframeContainer = document.querySelector("#visualiserDiv");
-	iframeContainer.contentWindow.document.open();
-	iframeContainer.contentWindow.document.write(sourceHtml);
-	iframeContainer.contentWindow.document.close();
-	
-    if((!errorFound) && verbose)
-    {
-        $("#warningSpan").text(" - Build Finis ! ")
-    }
+    COMPONENTS["intervalSelectInput"].on("change",(e)=>{
+        let interval = COMPONENTS["intervalSelectInput"].val();
+        if (interval == -1)
+        {
+            COMPONENTS["manualRefreshBtn"].show();
+            clearInterval(buildIntervalObject);
+        }
+        else
+        {
+            COMPONENTS["manualRefreshBtn"].hide();
+            clearInterval(buildIntervalObject);
+            buildIntervalObject = setInterval(refresh, interval);
+        }
+    })
 }
+
 
 /*****************************************************
 	Apply the codemirror to both textarea editor
 *****************************************************/
+// variable used to store the codemirror objects, only useful in build functions so far
+var textAreas = [
+	{
+        componentName:"htmlMirror",
+        textAreaId:"htmlEditor",
+        mode:"htmlmixed",
+    },
+    {
+        componentName:"cssMirror",
+        textAreaId:"cssEditor",
+        mode:"css",
+    }
+]
 
-// variable used to store the codemirror objects, only useful in 'build' function so far
-var mirrors = {};  
-var inputs = {
-	/*
-	idOfTextarea : codeMirror Mode 
-	*/
-	"htmlEditor": "htmlmixed",
-	"cssEditor": "css"
+function createMirrors()
+{
+    for (index in textAreas)
+    {
+        let input = textAreas[index]
+        let mirrorObj = CodeMirror.fromTextArea(document.getElementById(input.textAreaId), {
+            lineNumbers: true,
+            theme: "cobalt",
+            mode: input.mode,
+            tabSize: 2
+        });
+        addComponent(input.componentName, mirrorObj);
+    }
 }
-let inputId = Object.keys(inputs);
+/*********************** BUILD FUNCTIONS *************************/
+var needBuild = true;
+function forceNextRebuild() { needBuild = true; }
 
-for (keyId in inputId){
-	mirrors[inputId[keyId]] = CodeMirror.fromTextArea(document.getElementById(inputId[keyId]), {
-		extraKeys: {"Ctrl-Space": "autocomplete"},
-		lineNumbers: true,
-		theme: "cobalt",
-		mode: inputs[inputId[keyId]],
-		tabSize: 4
-	});  
+function buildHTML() 
+{
+    console.log("build...")
+    needBuild = false;
+    let htmlCode = COMPONENTS["htmlMirror"].getValue();
+
+	let parser = new DOMParser();
+    newDoc = parser.parseFromString(htmlCode, "text/html"); 
+
+    let sourceHtml = `<html>${newDoc.documentElement.innerHTML}</html>`
+	let iframeContainer = COMPONENTS["iframe"][0];
+	iframeContainer.contentWindow.document.open();
+	iframeContainer.contentWindow.document.write(sourceHtml);
+	iframeContainer.contentWindow.document.close();
 }
 
-/*
-	If we don't launch the build function, the iframe element its own parent code
-	(index.html), didn't find the reason so we build the exemple code to show the 
-	result
-*/
-build(false)
+function refresh() 
+{
+    let forceChecked = COMPONENTS["forceInjectionCheckbox"].prop("checked");
+    let rebuild = needBuild;
+    if (rebuild) buildHTML();
+
+    let cssCode = COMPONENTS["cssMirror"].getValue();
+    cssCode += getPluginsContent();
+
+    let iframeDoc = COMPONENTS["iframe"][0].contentWindow.document;
+    if ( (iframeDoc.querySelector("#cascadeCSS") == null) && forceChecked)
+    {
+		let cssDiv = iframeDoc.createElement("style");
+        cssDiv.setAttribute("id", "cascadeCss");
+        cssDiv.innerHTML = cssCode;
+		iframeDoc.querySelector("body").appendChild(cssDiv);
+    }
+    else if ( iframeDoc.querySelector("#cascadeCSS") != null )
+    {
+        iframeDoc.querySelector("#cascadeCss").innerHTML = cssCode;
+    }
+}
+
+/* INIT */
+addComponentBySelector("htmlTextArea", "#htmlEditor").val(DEFAULT_HTML);
+addComponentBySelector("cssTextArea", "#cssEditor").val(DEFAULT_CSS);
+addComponentBySelector("intervalSelectInput","#testIntervalSelect").val(-1);
+addComponentBySelector("manualRefreshBtn","#manualBuildButton").on('click', refresh);
+addComponentBySelector("forceInjectionCheckbox","#forceCssCheckBox").on("change", forceNextRebuild);
+addComponentBySelector("iframe","#visualiserDiv");
+createMirrors();
+initIntervalUI();
+// we change anything in the html, then we need to rebuild the page
+// else, we just refresh the css
+CodeMirror.on(COMPONENTS["htmlMirror"], "change", forceNextRebuild)
+refresh()
